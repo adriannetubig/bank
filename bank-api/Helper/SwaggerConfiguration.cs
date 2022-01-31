9b7d5c83cd90f292bbc34e9e9da7565cf33c6200
Adrianne Claude Tubig <adriannetubig@gmail.com>
2022-01-31 14:51:24 +0800

    Implemented swagger

---
+++ bank-api/Helper/SwaggerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace bank_api.Helper
+{
+    public static class SwaggerConfiguration
+    {
+        public static string SchemaIdStrategy(Type currentClass)
+        {
+            string returnedValue = currentClass.Name;
+            if (returnedValue.EndsWith("Dto"))
+                returnedValue = returnedValue.Replace("Dto", string.Empty);
+            return returnedValue;
+        }
+    }
+}
--- bank-api/Program.cs
+++ bank-api/Program.cs
@@ -1,8 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 bank_api.Helper.Dependencies.Register(builder.Services, builder.Configuration);
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(a =>
+{
+    a.CustomSchemaIds(bank_api.Helper.SwaggerConfiguration.SchemaIdStrategy);
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
