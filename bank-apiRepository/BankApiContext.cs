00bc4af32792bef8f756588e499e6f7755d02778
Adrianne Claude Tubig <adriannetubig@gmail.com>
2022-01-31 12:48:26 +0800

    Fixed issue on loading name ValueObject

--- bank-apiRepository/BankApiContext.cs
+++ bank-apiRepository/BankApiContext.cs
@@ -37,6 +37,11 @@ namespace bank_apiRepository
             {
                 a.ToTable("Customer").HasKey(b => b.Id);
                 a.Property(b => b.Id).HasColumnName("CustomerId");
+                a.OwnsOne(b => b.Name, b =>
+                {
+                    b.Property(pp => pp.First).HasColumnName("FirstName");
+                    b.Property(pp => pp.Last).HasColumnName("LastName");
+                });
             });
 
             modelBuilder.Entity<Transaction>(a =>
