eb5b67ae403e3e2551c9f68219d069501de2e489
Adrianne Claude Tubig <adriannetubig@gmail.com>
2022-01-31 12:50:47 +0800

    Make the Api project run

--- bank-api/Program.cs
+++ bank-api/Program.cs
@@ -1,7 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 bank_api.Helper.Dependencies.Register(builder.Services, builder.Configuration);
+
+builder.Services.AddControllers();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
 
+app.UseAuthorization();
+
+app.MapControllers();
 app.Run();
